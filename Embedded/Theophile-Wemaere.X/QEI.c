c074c1154e25624909cac535b41aa3b55a055101
Theophile_Wemaere <78029398+IUT-Theophile-Wemaere@users.noreply.github.com>
2021-06-12 15:24:32 +0000

    Update QEI.c

--- Embedded/Theophile-Wemaere.X/QEI.c
+++ Embedded/Theophile-Wemaere.X/QEI.c
@@ -45,7 +45,7 @@ void QEIUpdateData(void)
         reset=0;
     }
 
-    //On réactualise les valeurs des positions
+    //On rÃ©actualise les valeurs des positions
     long QEI1RawValue =POS1CNTL;
     QEI1RawValue+=((long)POS1HLD<<16);
 
@@ -60,18 +60,18 @@ void QEIUpdateData(void)
     delta_d = QeiDroitPosition - QeiDroitPosition_T_1; 
     delta_g = QeiGauchePosition - QeiGauchePosition_T_1; 
 
-    // delta_theta = atan((delta_d - detla_g) / DISTROUES);
+    // delta_theta = (delta_d - detla_g) / DISTROUES;
     delta_theta = (delta_d - delta_g)/(DISTROUES); 
     dx =(delta_d + delta_g)/2; 
 
     //calcul des vitesses
-    // attention à remultiplier par la fréquence d?échantillonnage
+    // attention Ã  remultiplier par la frÃ©quence d?Ã©chantillonnage
     robotState.vitesseDroitFromOdometry = delta_d*FREQ_ECH_QEI;
     robotState.vitesseGaucheFromOdometry = delta_g*FREQ_ECH_QEI;
     robotState.vitesseLineaireFromOdometry = (robotState.vitesseDroitFromOdometry + robotState.vitesseGaucheFromOdometry)/2 ;
     robotState.vitesseAngulaireFromOdometry = delta_theta*FREQ_ECH_QEI;
 
-    //Mise à jour du postionnement  terrain à t-1
+    //Mise Ã  jour du postionnement  terrain Ã  t-1
     robotState.xPosFromOdometry_1 = robotState.xPosFromOdometry ;
     robotState.yPosFromOdometry_1 = robotState.yPosFromOdometry ;
     robotState.angleRadianFromOdometry_1 = robotState.angleRadianFromOdometry ;
